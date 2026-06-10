f0604d5bd005fbb267ec9d73dbded606973b76b2
Azmara927 <amahil@students.mags.school.nz>
2026-06-11 00:48:54 +1200

    Halfway done with seamless run scene + created basic layout of death scene

--- byw/Scripts/running.gd
+++ byw/Scripts/running.gd
@@ -2,16 +2,34 @@ extends Node2D
 
 var score: int = 0
 
+@export var bg_1 : Node
+@export var bg_2 : Node
+@export var bg_3 : Node
+
+
+func _process(delta):
+	bg_1.global_position.x -= 50 * delta
+	bg_2.global_position.x -= 50 * delta
+	if bg_1.global_position.x <= 0:
+		bg_1.global_position.x = 620
+	if bg_2.global_position.x <= 0:
+		bg_2.global_position.x = 620
+	if bg_3.global_position.x <= 0:
+		bg_3.global_position.x <= 620
+
+
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if Input.is_action_pressed("ui_up"):
-		score += 1
-		print(score)
+#func _process(delta: float) -> void:
+#	if Input.is_action_pressed("ui_up"):
+#		score += 1
+#		print(score)
 
 
 
