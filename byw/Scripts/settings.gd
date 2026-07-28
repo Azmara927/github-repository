74ed3ddab574a10dca6e46debc880f7e09c486ae
Azmara927 <amahil@students.mags.school.nz>
2026-07-28 23:28:21 +1200

    SETTINGS COMPLETEEEE

--- byw/Scripts/settings.gd
+++ byw/Scripts/settings.gd
@@ -2,6 +2,8 @@ extends Control
 
 @onready var help: Control = $"."
 
+var master_bus_index = AudioServer.get_bus_index("Master")
+var SFX_bus_index = AudioServer.get_bus_index("SFX")
 
 
 # Called when the node enters the scene tree for the first time.
@@ -16,3 +18,11 @@ func _process(delta: float) -> void:
 
 func _on_button_pressed() -> void:
 	help.hide()
+
+
+func _on_master_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(master_bus_index, linear_to_db(value))
+
+
+func _on_sfx_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(SFX_bus_index, linear_to_db(value))
